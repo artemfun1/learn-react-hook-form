3418dbc048bf8d518c3214c2b3b93189fbb52936
artemfun1 <a.o.peshkov@yandex.ru>
2024-08-12 13:23:22 +0500

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,82 @@
-import { Controller, SubmitErrorHandler, SubmitHandler, useForm } from 'react-hook-form'
-import './App.css'
+import {
+	Controller,
+	SubmitErrorHandler,
+	SubmitHandler,
+	useForm,
+} from "react-hook-form";
+import "./App.css";
 
 interface IMyForm {
-  name:string;
-  age:number
+	name: string;
+	age: number;
 }
 
 function App() {
+	const {
+		register,
+		handleSubmit,
+		clearErrors,
+		formState: { errors },
+		reset,
+		setValue,
+		watch,
+		control,
+	} = useForm<IMyForm>({
+		defaultValues: {
+			age: 18,
+		},
+	});
 
-const {register, handleSubmit, clearErrors, formState:{errors}, reset, setValue,watch,control } = useForm<IMyForm>({
-  defaultValues:{
-    age:18
-  }
-})
+	const submit: SubmitHandler<IMyForm> = data => {
+		console.log(data);
+	};
 
-const submit:SubmitHandler<IMyForm> = data => {
-  console.log(data)
+	const error: SubmitErrorHandler<IMyForm> = data => {
+		console.log("error", data);
+	};
+
+	const isName = (data: string) => {
+		if (data) return true;
+	};
+
+	return (
+		<>
+			<form onSubmit={handleSubmit(submit, error)}>
+				<input
+					type="text"
+					{...register("name", { required: true, validate: isName })}
+					aria-invalid={errors.name ? true : false}
+				/>
+
+				<Controller
+					name="age"
+					control={control}
+					render={({ field }) => <input {...field} />}
+				/>
+
+				{/* <input type="number" {...register('age')} /> */}
+				<button>Send</button>
+				<button
+					type="button"
+					onClick={() =>
+						reset({
+							age: 0,
+							name: "",
+						})
+					}
+				>
+					Clear Form
+				</button>
+				<button type="button" onClick={() => clearErrors()}>
+					Clear Errors
+				</button>
+				<button type="button" onClick={() => setValue("name", "SetTestName")}>
+					Set Name
+				</button>
+			</form>
+			{watch("age")}
+		</>
+	);
 }
 
-const error:SubmitErrorHandler<IMyForm> = data => {
-  console.log('error',data)
-}
-
-const isName = (data:string) =>{
-  if(data) return true
-}
-
-
-
-  return (
-    <>
-      <form onSubmit={handleSubmit(submit,error)}>
-        <input type="text" {...register('name',{required:true , validate: isName})} aria-invalid={errors.name?true:false} />
-        <Controller
-        name='age'
-        control={control}
-        render={({field})=> <input {...field}/>}
-        
-        
-        />
-        {/* <input type="number" {...register('age')} /> */}
-        <button>Send</button>
-         <button type='button' onClick={()=>reset({
-          age:0,
-          name:''
-         })}>Clear Form</button>
-        <button type='button' onClick={()=>clearErrors()}>Clear Errors</button>
-        <button type='button' onClick={()=>setValue('name','SetTestName')}>Set Name</button>
-       
-      </form>
-      {watch('age')}
-    </>
-  )
-}
-
-export default App
+export default App;
